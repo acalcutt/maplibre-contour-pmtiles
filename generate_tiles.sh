dba88101095aef06cd4ad8087b3915c05fe3d06a
root <root@planetgen.wifidb.net>
2024-12-16 21:54:26 +0000

    fix bash script parameters

--- generate_tiles.sh
+++ generate_tiles.sh
@@ -5,16 +5,16 @@ process_tile() {
     local x_coord="$2"
     local y_coord="$3"
     local increment=10
-    local mazZoom=11
-    local sMazZoom=8
-    local sEncoding="terrarium"
-    local sFile="/mnt/c/Users/Andrew/Desktop/Junk/tile_data/pmtiles/gebco_terrarium0-8.pmtiles"
+    local maxZoom=12
+    local sMaxZoom=12
+    local sEncoding="mapbox"
+    local sFile="/work/JAXA_AW3D30_2024_terrainrgb_z0-Z12_png.pmtiles"
 
 
     echo "process_tile: Processing tile - Zoom: $zoom_level, X: $x_coord, Y: $y_coord"
 
-    npx tsx ./src/generate-countour-tile-batch.ts --x $x_coord --y $y_coord --z $zoom_level --maxZoom $mazZoom --sFile $mazZoom --sFile $sEncoding --sMaxZoom $sEncoding --increment $increment
-    
+    npx tsx ./src/generate-countour-tile-batch.ts --x $x_coord --y $y_coord --z $zoom_level --maxZoom $maxZoom --sFile $sFile --sEncoding $sEncoding --sMaxZoom $sMaxZoom --increment $increment
+
     echo "process_tile: Finished processing $zoom_level-$x_coord-$y_coord"
 }
 export -f process_tile
